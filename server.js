1f277db0e41c7b8e2657ee09c01a26bc0d3db9fe
Lokesh Shah <shahlokesh619@gmail.com>
2017-12-20 00:09:54 +0530

    My First Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err) =>{
+  if(err){
+    console.log('Unable to appaned server log');
+  }
+});
+  next();
+});
+
+app.use(function(req, res, next){
+ res.render('maintainance.hbs');
+} );
+hbs.registerHelper('getCurrentYear', function(){
+  return new Date ().getFullYear();
+});
+hbs.registerHelper('screamIt', function(text){
+  return text;
+});
+ app.get('/', function(req,res){
+   //res.send('Hello Express!');
+   res.render('home.hbs', {
+     pageTitle: 'Home Page',
+     WelcomeMessage: 'Welcome to my website'
+   });
+ });
+app.get('/about', function(req, res){
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+})
+app.get('/bad', function(req, res){
+  res.send({
+    errorMessage: 'Unable to fulfill the request'
+  })
+});
+app.listen(3000, function(){
+  console.log('Server is up and running');
+});
